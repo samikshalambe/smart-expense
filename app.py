0927aba167ac6b79c6b273e1e785af841a90a689
samikshalambe <lambesamiksha27@gmail.com>
2026-04-03 19:48:06 +0530

    Fix navbar duplication by removing duplicate st.markdown call in nav.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ for key, default in [("logged_in", False), ("username", None)]:
 if st.session_state["logged_in"]:
     # Temporarily disabled automatic redirect to avoid st.switch_page error
     # st.switch_page("pages/1_Dashboard.py")
-    navbar("Dashboard")  # Show navigation when logged in
+    pass  # Navigation is handled by individual pages
 else:
     st.markdown("<br><br>", unsafe_allow_html=True)
 
